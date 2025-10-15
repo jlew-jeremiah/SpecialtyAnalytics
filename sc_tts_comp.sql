f02c85cef0f84854baa65a94796c1de05d3e14e5
jlew-jeremiah <jeremiah.lewis@sysco.com>
2025-10-15 09:52:38 -0500

    Update sc_tts_comp.sql

--- sc_tts_comp.sql
+++ sc_tts_comp.sql
@@ -1,7 +1,3 @@
-/*
-This is a quick and dirty approach to getting the SC TTS compensation. Eventually, it will likely be phased out to simply be on a dashboard. 
-*/
-
 WITH user_input AS (
 	SELECT 
 	  9::int AS custom_start_fisc_wk_of_yr_nbr -- replace the 0 with e.g. 1 to override
@@ -59,12 +55,12 @@ WITH user_input AS (
 --	;
 )
 , base_TTS_data1 AS (
-	select distinct --count(*) /*
+	select --count(*) /*
 	a.bu,
 	a.bu_id,
 	a.rgn,
 	a.site_cust_id,
-	a.clstr_id,
+	coalesce(a.clstr_id,b.clstr_id) as clstr_id,
 	a.cust_ship_to_clstr_skey as clstr_skey,
 	a.lcl_acct_typ AS acct_typ,
 	upper(a.lcl_cust_nm) AS cust_nm,
@@ -72,13 +68,18 @@ WITH user_input AS (
 	upper(coalesce(scd.sales_cd,sc.sc_user_id,sf.sc_saml_federation_id)) as sc_id,
 	upper(a.dsm) as dsm,
 	w.fisc_yr_skey,
+	w.fisc_wk_id,
 	-- BU cluster totals
 	case when a.bu_id=3 then a.pcs_sold_qty else a.cs_eq_sold_qty end as uom,
-	fp_qty AS fp_cluster_cases_sold,
-	bn_qty AS mg_cluster_cases_sold,
-	bl_qty AS bl_cluster_cases_sold,
+	max(fp_qty_ty) over (partition by b.clstr_skey) as fp_cluster_cases_sold_ty,
+	max(fp_qty_ly) over (partition by b.clstr_skey) as fp_cluster_cases_sold_ly,
+	max(bn_qty_ty) over (partition by b.clstr_skey) as mg_cluster_pieces_sold_ty,
+	max(bn_qty_ly) over (partition by b.clstr_skey) as mg_cluster_pieces_sold_ly,
+	max(bl_qty_ty) over (partition by b.clstr_skey) as bl_cluster_cases_sold_ty,
+	max(bl_qty_ly) over (partition by b.clstr_skey) as bl_cluster_cases_sold_ly
 	-- Cluster total excluding self
-	case a.bu_id when 1 then clstr_qty_sold_ex_bl when 2 then clstr_qty_sold_ex_fp when 3 then clstr_qty_sold_ex_bn else 0 end as cluster_cases_sold_excl_self --*/
+--	case a.bu_id when 2 then clstr_qty_sold_ex_fp_ty when 3 then clstr_qty_sold_ex_bn_ty else 0 end as cluster_cases_sold_excl_self_ty,
+--	case a.bu_id when 2 then clstr_qty_sold_ex_fp_ly when 3 then clstr_qty_sold_ex_bn_ly else 0 end as cluster_cases_sold_excl_self_ly--*/
 	FROM ts_spcl_edwp.tts2_clstr_sale_head_fact a
 	left join ts_spcl_edwp.tts2_sc sc on a.site_cust_id=sc.acct_id and a.bu_id in (2,3)  
 	left join ts_spcl_edwp.sf_hierarchy sf on a.site_cust_id=sf.acct_id and a.bu_id in (2,3)  
@@ -89,14 +90,20 @@ WITH user_input AS (
 	    ON a.fisc_wk_id = w.fisc_wk_id
 	   AND w.fisc_yr_skey IN (fd.current_fisc_yr, fd.current_fisc_yr - 1)
 	   AND w.fisc_wk_of_yr_nbr BETWEEN wr.start_fisc_wk_of_yr_nbr AND wr.end_fisc_wk_of_yr_nbr
-	LEFT JOIN (
-		select t.cust_ship_to_clstr_skey as clstr_skey,w.fisc_yr_skey as fisc_yr_skey
-		,sum(case bu_id when 3 then pcs_sold_qty when 2 then cs_eq_sold_qty else 0 end) clstr_qty_sold_ex_bl --pcs for bn, cs_eq for all else 
-		,sum(case bu_id when 3 then pcs_sold_qty when 1 then cs_eq_sold_qty else 0 end) clstr_qty_sold_ex_fp
-		,sum(case bu_id when 1 then cs_eq_sold_qty when 2 then cs_eq_sold_qty else 0 end) clstr_qty_sold_ex_bn
-		,sum(case when bu_id=1 then cs_eq_sold_qty else 0 end) bl_qty
-		,sum(case when bu_id=2 then cs_eq_sold_qty else 0 end) fp_qty
-		,sum(case when bu_id=3 then pcs_sold_qty else 0 end) bn_qty
+	right JOIN (
+		select t.cust_ship_to_clstr_skey as clstr_skey,t.clstr_id
+--		,sum(case when w.fisc_yr_skey=fd.current_fisc_yr   then case bu_id when 3 then pcs_sold_qty when 2 then cs_eq_sold_qty else 0 end else 0 end) clstr_qty_sold_ex_bl_ty --pcs for bn, cs_eq for all else 
+--		,sum(case when w.fisc_yr_skey=fd.current_fisc_yr-1 then case bu_id when 3 then pcs_sold_qty when 2 then cs_eq_sold_qty else 0 end else 0 end) clstr_qty_sold_ex_bl_ly
+--		,sum(case when w.fisc_yr_skey=fd.current_fisc_yr   then case bu_id when 3 then pcs_sold_qty when 1 then cs_eq_sold_qty else 0 end else 0 end) clstr_qty_sold_ex_fp_ty
+--		,sum(case when w.fisc_yr_skey=fd.current_fisc_yr-1 then case bu_id when 3 then pcs_sold_qty when 1 then cs_eq_sold_qty else 0 end else 0 end) clstr_qty_sold_ex_fp_ly
+--		,sum(case when w.fisc_yr_skey=fd.current_fisc_yr   then case bu_id when 1 then cs_eq_sold_qty when 2 then cs_eq_sold_qty else 0 end else 0 end) clstr_qty_sold_ex_bn_ty
+--		,sum(case when w.fisc_yr_skey=fd.current_fisc_yr-1 then case bu_id when 1 then cs_eq_sold_qty when 2 then cs_eq_sold_qty else 0 end else 0 end) clstr_qty_sold_ex_bn_ly
+		,sum(case when w.fisc_yr_skey=fd.current_fisc_yr   then case when bu_id=1 then cs_eq_sold_qty else 0 end else 0 end) bl_qty_ty
+		,sum(case when w.fisc_yr_skey=fd.current_fisc_yr-1 then case when bu_id=1 then cs_eq_sold_qty else 0 end else 0 end) bl_qty_ly
+		,sum(case when w.fisc_yr_skey=fd.current_fisc_yr   then case when bu_id=2 then cs_eq_sold_qty else 0 end else 0 end) fp_qty_ty
+		,sum(case when w.fisc_yr_skey=fd.current_fisc_yr-1 then case when bu_id=2 then cs_eq_sold_qty else 0 end else 0 end) fp_qty_ly
+		,sum(case when w.fisc_yr_skey=fd.current_fisc_yr   then case when bu_id=3 then pcs_sold_qty else 0 end else 0 end) bn_qty_ty
+		,sum(case when w.fisc_yr_skey=fd.current_fisc_yr-1 then case when bu_id=3 then pcs_sold_qty else 0 end else 0 end) bn_qty_ly
 		from ts_spcl_edwp.tts2_clstr_sale_head_fact t
 		CROSS JOIN fiscDay fd
 		CROSS JOIN fisc_wks_to_run AS wr
@@ -104,15 +111,20 @@ WITH user_input AS (
 	    ON t.fisc_wk_id = w.fisc_wk_id
 	   	AND w.fisc_yr_skey IN (fd.current_fisc_yr, fd.current_fisc_yr - 1)
 	   	AND w.fisc_wk_of_yr_nbr BETWEEN wr.start_fisc_wk_of_yr_nbr AND wr.end_fisc_wk_of_yr_nbr 
-	   	and t.lcl_acct_typ in ('TRS','TRP','LCC','BID','OTH')
+		/* and t.acct_typ in ('TRS','TRP','LCC','BID','OTH') -- this is excluded because all */
 		group by 1,2
 	) b  
     ON  a.cust_ship_to_clstr_skey = b.clstr_skey 
-    AND w.fisc_yr_skey = b.fisc_yr_skey
-   where a.bu_id in (2,3) 
-   and a.lcl_acct_typ in ('TRS','TRP','LCC','BID','OTH')
+   where 
+   (
+   	(a.bu_id in (2,3) 
+   	and 
+   	a.lcl_acct_typ in ('TRS','TRP','LCC','BID','OTH')
+   	)
+   	or a.cust_ship_to_clstr_skey is null -- 
+   )
 	-------------------------------------------------------------------------------------------------------
---	and a.clstr_id='FL000307'
+--	and a.clstr_id='CA012572' and a.bu_id=3
 --	and a.clstr_id='CA019429' --and rt.bu='BL'
 --	and a.clstr_id='TX110055'
 	-------------------------------------------------------------------------------------------------------
@@ -120,27 +132,36 @@ WITH user_input AS (
 --	;
 )
 ,base_tts_data as (
-select bu
-,bu_id
-,max(rgn) as rgn
-,clstr_id
-,clstr_skey
-,max(sc) as sc
-,sc_id as sc_id
-,max(dsm) as dsm
-,fisc_yr_skey
-,listagg(distinct site_cust_id,' / ') within group (order by site_cust_id) as site_cust_id
-,listagg(distinct acct_typ,' / ') within group (order by site_cust_id) as acct_typ
-,listagg(distinct upper(cust_nm),' / ') within group (order by site_cust_id) as cust_nm
-,sum(uom) as uom
-,max(fp_cluster_cases_sold) as fp_cluster_cases_sold
-,max(mg_cluster_cases_sold) as mg_cluster_cases_sold
-,max(bl_cluster_cases_sold) as bl_cluster_cases_sold
-,max(cluster_cases_sold_excl_self) as cluster_cases_sold_excl_self
-from base_tts_data1
-where sc_id is not null 
-group by 1,2/*,3*/,4,5/*,6*/,7/*,8*/,9
---;
+	select bu
+	,bu_id
+	,clstr_id
+	,clstr_skey
+	,sc_id as sc_id
+	,max(rgn) as rgn
+	,max(sc) as sc
+	,max(dsm) as dsm
+	,listagg(distinct site_cust_id,' / ') within group (order by site_cust_id) as site_cust_id
+	,listagg(distinct acct_typ,' / ') within group (order by site_cust_id) as acct_typ
+	,listagg(distinct upper(cust_nm),' / ') within group (order by site_cust_id) as cust_nm
+	,sum(case when fisc_yr_skey=fd.current_fisc_yr   then uom else 0 end) as uom_ty
+	,sum(case when fisc_yr_skey=fd.current_fisc_yr-1 then uom else 0 end) as uom_ly
+	,max(fp_cluster_cases_sold_ty) as fp_cluster_cases_sold_ty
+	,max(fp_cluster_cases_sold_ly) as fp_cluster_cases_sold_ly
+	,max(mg_cluster_pieces_sold_ty) as mg_cluster_pieces_sold_ty
+	,max(mg_cluster_pieces_sold_ly) as mg_cluster_pieces_sold_ly
+	,max(bl_cluster_cases_sold_ty) as bl_cluster_cases_sold_ty
+	,max(bl_cluster_cases_sold_ly) as bl_cluster_cases_sold_ly
+--	,max(cluster_cases_sold_excl_self_ty) as cluster_cases_sold_excl_self_ty
+--	,max(cluster_cases_sold_excl_self_ly) as cluster_cases_sold_excl_self_ly
+	from base_tts_data1
+	CROSS JOIN fiscDay fd
+	where sc_id is not null 
+	---------------------------------------------------------------------------------------
+--	and clstr_id in ('TX060331')
+	---------------------------------------------------------------------------------------
+	group by 1,2,3,4,5
+--	order by clstr_id 
+--	;
 )
 ,qual_sc as (
 	select max(region) region, sc.sales_cd,max(sc.sales_nm) as sales_nm,max(sales_job_classification) as sales_job_classification
@@ -188,21 +209,24 @@ group by 1,2/*,3*/,4,5/*,6*/,7/*,8*/,9
 ,sc_d_new as (
 	select bu
 	,bu_id
-	,max(rgn) as rgn
 	,clstr_id
 	,clstr_skey
+	,sc_id 
+	,max(rgn) as rgn
 	,max(dsm_nm) as dsm_nm
 	,max(sc) as sc
-	,sc_id 
-	,fisc_yr_skey
+--	,fisc_yr_skey
 	,listagg(distinct site_cust_id,' / ') within group (order by site_cust_id) as site_cust_id 
 	,listagg(distinct acct_typ,' / ') within group (order by site_cust_id) as acct_typ
 	,listagg(distinct cust_nm,' / ') within group (order by site_cust_id) as cust_nm
 	,listagg(distinct data_source,' / ') within group (order by site_cust_id) as data_source
-	,sum(uom) as uom
+--	,sum(uom) as uom
 	from sc_d_new1 
 	where sc_id is not null 
-	group by 1,2,/*3,*/4,5/*,6,7*/,8,9
+	---------------------------------------------------------------------------------
+--	and 
+	---------------------------------------------------------------------------------
+	group by 1,2,3,4,5
 --	;
 )
 , almost AS (
@@ -211,29 +235,32 @@ group by 1,2/*,3*/,4,5/*,6*/,7/*,8*/,9
 	COALESCE(sc.bu_id, rt.bu_id) AS bu,
 	COALESCE(sc.rgn, rt.rgn) AS rgn,
 	coalesce(sc.clstr_id,rt.clstr_id) as clstr_id,
+	upper(COALESCE(sc.sc,rt.sc,hr.emple_medm_desc,hr1.emple_medm_desc)) AS sc,
+	upper(coalesce(sc.sc_id,rt.sc_id)) as sc_id,
+	coalesce(hr.emple_nbr,hr1.emple_nbr) AS sc_workday_nbr,
+	CASE WHEN rt.sc IS NOT NULL THEN 'TTS' ELSE 'SC Dash.' END AS data_source,
+	max(upper(COALESCE(sc.dsm_nm,rt.dsm)))  AS dsm,
 	max(coalesce(sc.site_cust_id,rt.site_cust_id)) AS site_cust_id,
 	max(coalesce(sc.acct_typ,rt.acct_typ)) as acct_typ,
 	max(coalesce(sc.cust_nm,rt.cust_nm)) as cust_nm,
-	upper(COALESCE(sc.sc,rt.sc,hr.emple_medm_desc,hr1.emple_medm_desc)) AS sc,
-	upper(coalesce(sc.sc_id,rt.sc_id)) as sc_id,
-	max(upper(COALESCE(sc.dsm_nm,rt.dsm)))  AS dsm,
-	coalesce(hr.emple_nbr,hr1.emple_nbr) AS sc_workday_nbr,
-	CASE WHEN rt.sc IS NOT NULL THEN 'TTS' ELSE 'SC Dash.' END AS data_source,
-	coalesce(sc.fisc_yr_skey,rt.fisc_yr_skey) as fisc_yr,
-	sum(COALESCE(rt.UOM,0)) AS UOM, --leave out sc.uom
-	max(COALESCE(fp_cluster_cases_sold,0))  AS fp_cluster_cases_sold,
-	max(COALESCE(bl_cluster_cases_sold,0))  AS bl_cluster_cases_sold,
-	max(COALESCE(mg_cluster_cases_sold,0))  AS mg_cluster_cases_sold,
-	max(COALESCE(cluster_cases_sold_excl_self,0)) AS cluster_cases_sold_excl_self --*/
+	max(COALESCE(rt.uom_ty,0)) AS UOM_ty, --leave out sc.uom
+	max(COALESCE(rt.uom_ty,0)) AS UOM_ly, 
+	max(COALESCE(fp_cluster_cases_sold_ty,0))  AS fp_cluster_cases_sold_ty,
+	max(COALESCE(fp_cluster_cases_sold_ly,0))  AS fp_cluster_cases_sold_ly,
+	max(COALESCE(bl_cluster_cases_sold_ty,0))  AS bl_cluster_cases_sold_ty,
+	max(COALESCE(bl_cluster_cases_sold_ly,0))  AS bl_cluster_cases_sold_ly,
+	max(COALESCE(mg_cluster_pieces_sold_ty,0))  AS mg_cluster_pieces_sold_ty,
+	max(COALESCE(mg_cluster_pieces_sold_ly,0))  AS mg_cluster_pieces_sold_ly
+	--*/
 	FROM base_TTS_data AS rt
-	left /*full*//*right*/ outer join SC_D_new AS sc ON rt.clstr_skey=sc.clstr_skey AND rt.fisc_yr_skey=sc.fisc_yr_skey and rt.bu_id=sc.bu_id and rt.sc_id=sc.sc_id
+	left /*full*//*right*/ outer join SC_D_new AS sc ON rt.clstr_skey=sc.clstr_skey and rt.bu_id=sc.bu_id and rt.sc_id=sc.sc_id
 	LEFT JOIN edwp.org_emple_hr_mstr_dim AS hr ON sc.sc_id = hr.emple_user_nm AND hr.curr_rec_ind = 'Y' AND hr.emple_user_nm <> ''
 	LEFT JOIN edwp.org_emple_hr_mstr_dim AS hr1 ON rt.sc_id = hr1.emple_user_nm AND hr1.curr_rec_ind = 'Y' AND hr1.emple_user_nm <> ''
 	WHERE upper(COALESCE(sc.sc, rt.sc)) NOT IN ('ENTERPRISE APIUSER', '')
 	------------------------------------------------------------------------------
---	and coalesce(sc.clstr_id,rt.clstr_id)= 'CA003082' 
+--	and coalesce(sc.clstr_id,rt.clstr_id)= 'CA012572' 
 	------------------------------------------------------------------------------
-	GROUP BY 1,2,3,7,8,/*9,*/10,11,12
+	GROUP BY 1,2,3,4,5,6,7
 --	;
 )
 ,so_close as (
@@ -246,27 +273,28 @@ group by 1,2/*,3*/,4,5/*,6*/,7/*,8*/,9
 	,qual_sc.qty_ly
 	,qual_sc.qty_diff
 	,qual_sc.qual 
-	,max(site_cust_id) as site_cust_id
-	,max(acct_typ) as acct_typ
-	,max(cust_nm) as cust_nm
-	,max(sc) as sc
-	,max(dsm) as dsm
-	,max(data_source) as naming_data_source
-	,sum(case when fisc_yr=f.current_fisc_yr   then uom else 0 end) as uom_ty
-	,sum(case when fisc_yr=f.current_fisc_yr-1 then uom else 0 end) as uom_ly
-	,sum(case when fisc_yr=f.current_fisc_yr   then fp_cluster_cases_sold else 0 end) as fp_cluster_cases_sold_ty
-	,sum(case when fisc_yr=f.current_fisc_yr-1 then fp_cluster_cases_sold else 0 end) as fp_cluster_cases_sold_ly
-	,sum(case when fisc_yr=f.current_fisc_yr   then bl_cluster_cases_sold else 0 end) as bl_cluster_cases_sold_ty
-	,sum(case when fisc_yr=f.current_fisc_yr-1 then bl_cluster_cases_sold else 0 end) as bl_cluster_cases_sold_ly
-	,sum(case when fisc_yr=f.current_fisc_yr   then mg_cluster_cases_sold else 0 end) as mg_cluster_cases_sold_ty
-	,sum(case when fisc_yr=f.current_fisc_yr-1 then mg_cluster_cases_sold else 0 end) as mg_cluster_cases_sold_ly
-	,sum(case when fisc_yr=f.current_fisc_yr   then cluster_cases_sold_excl_self else 0 end) as cluster_cases_sold_excl_self_ty
-	,sum(case when fisc_yr=f.current_fisc_yr-1 then cluster_cases_sold_excl_self else 0 end) as cluster_cases_sold_excl_self_ly
+	,site_cust_id as site_cust_id
+	,acct_typ as acct_typ
+	,cust_nm as cust_nm
+	,sc as sc
+	,dsm as dsm
+	,data_source as naming_data_source
+--	,uom_ty as sc_uom_ty
+--	,uom_ly as sc_uom_ly
+	,case when bu=2 then fp_cluster_cases_sold_ty when bu=3 then mg_cluster_pieces_sold_ty end as uom_ty
+	,case when bu=2 then fp_cluster_cases_sold_ly when bu=3 then mg_cluster_pieces_sold_ly end as uom_ly
+	,fp_cluster_cases_sold_ty as fp_cluster_cases_sold_ty
+	,fp_cluster_cases_sold_ly as fp_cluster_cases_sold_ly
+	,mg_cluster_pieces_sold_ty as mg_cluster_pieces_sold_ty
+	,mg_cluster_pieces_sold_ly as mg_cluster_pieces_sold_ly
+	,bl_cluster_cases_sold_ty as bl_cluster_cases_sold_ty
+	,bl_cluster_cases_sold_ly as bl_cluster_cases_sold_ly
+	,fp_cluster_cases_sold_ty+mg_cluster_pieces_sold_ty+bl_cluster_cases_sold_ty-case when bu=2 then fp_cluster_cases_sold_ty when bu=3 then mg_cluster_pieces_sold_ty end as ttl_ex_self_ty
+	,fp_cluster_cases_sold_ly+mg_cluster_pieces_sold_ly+bl_cluster_cases_sold_ly-case when bu=2 then fp_cluster_cases_sold_ly when bu=3 then mg_cluster_pieces_sold_ly end as ttl_ex_self_ly
 	from almost
 	join qual_sc on almost.sc_id=qual_sc.sales_cd --and qual=true 
 	cross join fiscday f 
---	where clstr_id='CA003082' 
-	group by 1,2,3,4,5,6,7,8,9
+--	where clstr_id='TX060331' 
 --	order by 3,4
 --	;
 )
@@ -295,28 +323,27 @@ SELECT fc.bu
 ,qty_ty as ttl_vol_ty
 ,qty_ly as ttl_vol_ly
 ,qty_diff as ttl_diff_ty
-,uom_ty AS "Volume TY",
-uom_ly AS "Volume LY",
-uom_ty-uom_ly AS "Volume Growth",
-uom_ty>uom_ly AS "Growth Eligibility_old",
+--,sc_uom_ty as "SC Cluster Volume TY"
+--,sc_uom_ly as "SC Cluster Volume LY"
+--,sc_uom_ty-sc_uom_ly as "SC Cluster Volume Growth"
+,uom_ty AS "BU Cluster Volume TY",
+uom_ly AS "BU Cluster Volume LY",
+uom_ty-uom_ly AS "BU Cluster Volume Growth",
 fp_cluster_cases_sold_ty AS "Shared FP CS TY",
 fp_cluster_cases_sold_ly AS "Shared FP CS LY",
 fp_cluster_cases_sold_ty -fp_cluster_cases_sold_ly AS "Shared FP CS Growth",
+mg_cluster_pieces_sold_ty AS "Shared B|N pcs TY",
+mg_cluster_pieces_sold_ly AS "Shared B|N pcs LY",
+mg_cluster_pieces_sold_ty-mg_cluster_pieces_sold_ly AS "Shared B|N pcs Growth",
 bl_cluster_cases_sold_ty AS "Shared BL CS TY",
 bl_cluster_cases_sold_ly AS "Shared BL CS LY",
 bl_cluster_cases_sold_ty-bl_cluster_cases_sold_ly AS "Shared BL CS Growth",
-mg_cluster_cases_sold_ty AS "Shared B|N CS TY",
-mg_cluster_cases_sold_ly AS "Shared B|N CS LY",
-bl_cluster_cases_sold_ty-bl_cluster_cases_sold_ly AS "Shared B|N CS Growth",
-cluster_cases_sold_excl_self_ty AS "Total Shared CS TY",
-cluster_cases_sold_excl_self_ly AS "Total Shared CS LY",
-cluster_cases_sold_excl_self_ty-cluster_cases_sold_excl_self_ly AS "Total Shared CS Growth",
-CASE WHEN "Total Shared CS Growth" > 0 THEN TRUE ELSE FALSE END AS "TTS Growth Eligibility_old",
-CASE WHEN "Total Shared CS Growth" > 0 AND "Volume Growth" > 0 THEN "Total Shared CS Growth" * 0.2 ELSE 0 END AS "TTS Bonus_old"
+ttl_ex_self_ty AS "Total Shared Excl BU TY",
+ttl_ex_self_ly AS "Total Shared Excl BU LY",
+ttl_ex_self_ty-ttl_ex_self_ly AS "Total Shared Growth"
 FROM so_close AS fc
---CROSS JOIN fiscDay AS fd
 LEFT join (select distinct market,rgn,rgn_index from ts_spcl_edwp.site_details where bu_id<>1) AS sd ON fc.rgn=sd.rgn_index
 --where clstr_id='PA087311'
---order by 1,3,4,5
+order by 1,3,4,5
 --) select sc_id,clstr_id,count(*) cnt from fnl group by 1,2 having count(*)>1 order by cnt desc 
 ;
